85203f04f4a138b9097fd7c3b72dcff18fc0836d
EnriqueTests <enriquenada@outlook.es>
2021-07-08 12:00:15 -0500

    Desestructurar objeto

---
+++ destructuring-object/app.js
@@ -0,0 +1,32 @@
+// faster/easier way to access/unpack values from arrays
+
+const bob = {
+    first: 'bob',
+    last: 'sanders',
+    city: 'chicago',
+    siblings: {
+      sister: 'jane',
+    },
+  };
+  const {
+    first: firstName,
+    last,
+    city,
+    zip,
+    siblings: { sister: favoriteSibling },
+  } = bob;
+  console.log(firstName, last, city, zip, favoriteSibling);
+  
+  //
+  const firstNames = bob.first;
+  const lastName = bob.last;
+  const sister = bob.siblings.sister;
+  
+  console.log(firstNames,lastName,sister);
+  //
+  
+  function printPerson(person) {
+    console.log(person.first);
+  }
+  
+  printPerson(bob);
